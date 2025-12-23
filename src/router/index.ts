a61e74b888bfff8f40746ee35b522d5f00b8af8e
fMuchka <f.muchka@gmail.com>
2025-12-23 14:52:09 +0100

    F!! route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Routes from './Routes';
+import { useAuth } from '../composables/useAuth';
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: () => import('../pages/Home.vue') },
-    { path: '/login', component: () => import('../pages/Login.vue') },
+    { path: Routes.Home, component: () => import('../pages/Home.vue') },
+    { path: Routes.Login, component: () => import('../pages/Login.vue') },
   ],
-})
+});
+
+const { isAuthenticated } = useAuth();
+
+router.beforeEach((to, _from, next) => {
+  if (to.path !== Routes.Login && !isAuthenticated.value) next({ path: Routes.Login });
+  else next();
+});
