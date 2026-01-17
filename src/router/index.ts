1657ef65b4fbee9aabcbd7552e1309961e822542
fMuchka <f.muchka@gmail.com>
2026-01-17 21:36:54 +0100

    F** session route

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,14 +7,21 @@ export const router = createRouter({
   routes: [
     { path: Routes.Home, component: () => import('../pages/Home.vue') },
     { path: Routes.Login, component: () => import('../pages/Login.vue') },
+    { path: Routes.Join, component: () => import('../pages/Join.vue') },
+    { path: Routes.Session, component: () => import('../pages/Session.vue') },
   ],
 });
 
 router.beforeEach((to, _from, next) => {
   const { isAuthenticated } = useAuth(); // ✅ Fresh check on each navigation
-  if (to.path !== Routes.Login && !isAuthenticated.value) {
-    next({ path: Routes.Login });
-  } else {
+
+  if (to.path === Routes.Join || to.path === Routes.Session) {
     next();
+  } else {
+    if (to.path !== Routes.Login && !isAuthenticated.value) {
+      next({ path: Routes.Login });
+    } else {
+      next();
+    }
   }
 });
