b2e6cffdd52997b3d05bd90441aa5d8eab6d9a92
fMuchka <f.muchka@gmail.com>
2026-03-21 16:56:43 +0100

    F** SongListFlatView init

---
+++ src/components/song-list/flat-view/SongListFlatView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import type { Song } from '../../../lib/song';
+
+  defineProps<{
+    songs: Song[];
+  }>();
+</script>
+
+<template>
+  <ul class="song-list__flat song-list__list-reset">
+    <li
+      v-for="song in songs"
+      :key="song.id"
+      class="song-list__item"
+    >
+      {{ song.artist }} - {{ song.title }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  @import '../SongListShared.css';
+
+  .song-list__flat {
+    display: grid;
+    gap: var(--space-xs);
+  }
+</style>
